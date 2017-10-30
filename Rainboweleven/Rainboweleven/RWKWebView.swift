3df1e68b4049ea47dcde66ce2b8ce7b357cf0f61
chenshaomou <175144462@qq.com>
2017-10-30 17:42:29 +0800

    异步调用

--- Rainboweleven/Rainboweleven/RWKWebView.swift
+++ Rainboweleven/Rainboweleven/RWKWebView.swift
@@ -21,6 +21,11 @@ class RWKWebView: WKWebView ,RWebViewProtocol,WKUIDelegate,WKNavigationDelegate{
     
     weak open var ruiDelegate: WKUIDelegate?
     
+    //
+    var g_r_last_call_time:UInt64 = 0;
+    var g_r_js_cache = "";
+    var g_r_have_pending = false;
+    
     override init(frame: CGRect, configuration: WKWebViewConfiguration) {
         let js = "_jswk='_jsbridge=';".appending(RWebView.INIT_SCRIPT)
         let script = WKUserScript(source: js, injectionTime: WKUserScriptInjectionTime.atDocumentStart, forMainFrameOnly: true)
@@ -61,21 +66,37 @@ class RWKWebView: WKWebView ,RWebViewProtocol,WKUIDelegate,WKNavigationDelegate{
     }
     
     func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (String?) -> Void) {
-        
         let _prefix = "_jsbridge="
         let _jsinited = "_jsinited"
-        
         if prompt.hasPrefix(_jsinited){
             //TODO: jsbridge 完成初始化
             completionHandler("")
         }else if prompt.hasPrefix(_prefix){
             let method = String(prompt.suffix(from: _prefix.endIndex))
             guard let _dic = defaultText?.stringToDictionary() else { return }
-            if _dic["_jscbstub"] != nil{
+            if let _jscallback = _dic["_jscbstub"]{
                 //异步调用
+                //马上返回
+                completionHandler("")
+                let _value = RWebkitPluginsHub.shared.runPlugin(name: method, args: _dic).addingPercentEncoding(withAllowedCharacters: .whitespacesAndNewlines)
+                let _del = "delete window.\(_jscallback)"
+                let _js = "try {\(_jscallback)(decodeURIComponent(\"\(_value)\"));\(_del); } catch(e){};"
+                objc_sync_enter(webView)
+                let _now:UInt64 = UInt64(Date().timeIntervalSince1970 * 1000)
+                g_r_js_cache = "\(g_r_js_cache)\(_js)"
+                var _delay = 0.0
+                if(_now - g_r_last_call_time < 50 && g_r_have_pending == false){
+                    _delay = 50.0
+                    g_r_have_pending = true
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + _delay/1000.0 , execute: {
+                    
+                })
+                
+                objc_sync_exit(webView)
             }else{
                 //同步调用
-                let result = RWebkitPluginsHub.shared.runPluginSync(name: method, args: _dic)
+                let result = RWebkitPluginsHub.shared.runPlugin(name: method, args: _dic)
                 //TODO: 处理报错
                 completionHandler(result)
             }
@@ -84,7 +105,6 @@ class RWKWebView: WKWebView ,RWebViewProtocol,WKUIDelegate,WKNavigationDelegate{
         }else{
             //TODO: 普通的 js prompt
         }
-        
     }
     
 }
--- Rainboweleven/Rainboweleven/RWebkitPluginsHub.swift
+++ Rainboweleven/Rainboweleven/RWebkitPluginsHub.swift
@@ -8,73 +8,42 @@
 
 import Foundation
 
-typealias JsCallback = (_ result : String?)->Void
-typealias AsyncPluginAction = (_ args:[String:Any],_ jsCallback:JsCallback) ->Void
-typealias SyncPluginAction = (_ args:[String:Any]) -> String
+typealias PluginAction = (_ args:[String:Any]) -> String
 
 class RWebkitPlugin {
     let name : String
-    var asyncAction : AsyncPluginAction?
-    var syncAction : SyncPluginAction?
+    let action : PluginAction
     
-    init(_ n:String,_ a:@escaping AsyncPluginAction) {
+    init(_ n:String,_ a:@escaping PluginAction) {
         self.name = n
-        self.asyncAction = a
+        self.action = a
     }
-    
-    init(_ n:String,_ a:@escaping SyncPluginAction) {
-        self.name = n
-        self.syncAction = a
-    }
+
 }
 
 //插件管理器，单例实现
 class RWebkitPluginsHub {
     
     static let shared = RWebkitPluginsHub.init()
-    var asyncPlugins = [String : RWebkitPlugin]() //存储异步插件
-    var syncPlugins = [String : RWebkitPlugin]() //存储同步插件
+    var plugins = [String : RWebkitPlugin]()
     
     private init(){
         //添加存储插件
-        self.addASyncPlugin(name: "setValue") { (args, jsCallback) in
-            NSLog("setValue...")
-            jsCallback("")
-        }
         
-        self.addSyncPlugin(name: "getValue") { (args) -> String in
-            return "Hello"
+        self.addPlugin(name: "getValue") { (args) -> String in
+            return "Hello,\(args.dictionaryToJSONString())"
         }
     }
     
-    func addASyncPlugin(name:String,action:@escaping AsyncPluginAction ) {
-        asyncPlugins[name] = RWebkitPlugin(name,action)
+    func addPlugin(name:String,action:@escaping PluginAction ) {
+        plugins[name] = RWebkitPlugin(name,action)
     }
     
-    func addSyncPlugin(name:String,action:@escaping SyncPluginAction){
-        syncPlugins[name] = RWebkitPlugin(name,action)
-    }
-    
-    func runPluginSync(name:String,args:[String:Any]) -> String {
+    func runPlugin(name:String,args:[String:Any]) -> String {
         //TODO: 不存在，报错
-        guard let _plugin = syncPlugins[name] else { return ""}
-        if let _syncAction = _plugin.syncAction{
-            return _syncAction(args)
-        }else{
-            //TODO: 不存在，报错
-            return ""
-        }
+        guard let _plugin = plugins[name] else { return ""}
+        return _plugin.action(args)
     }
-    
-    func runPluginASync(name:String,args:[String:Any],jsCallback:JsCallback){
-        guard let _plugin = asyncPlugins[name] else { return}
-        if let _asyncAction = _plugin.asyncAction{
-            _asyncAction(args,jsCallback)
-        }else{
-            //TODO: 不存在，报错
-        }
-    }
-    
     
     
 }
