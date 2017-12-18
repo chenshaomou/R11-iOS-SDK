43659e7c0bdc5b1a4f5a85fc3572a195de1186b8
zhang <Angus@ZhangdeMacBook-Air.local>
2017-12-18 09:18:23 +0800

    添加Almofire引入

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # R11-iOS-SDK
 
-## 项目支持github
+## 项目支持Carthage引入
+
+### Carthage安装
+
+```SHELL
+$ brew update
+$ brew install carthage
+
 ```SHELL
 github "chenshaomou/R11-iOS-SDK" "master"
+
+## 该项目需要进入第三库 Alamofire
+
+建议使用carthage
+```SHELL
+github "Alamofire/Alamofire" ~> 4.5
+
---
+++ Rainboweleven/Rainboweleven/plugin/NetworkPlugin.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkPlugin.swift
+//  Rainboweleven
+//
+//  Created by Zhang Zhang on 12/18/17.
+//  Copyright © 2017 chenshaomou. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+public class NetWorking {
+    
+    func runAlmofire() {
+        
+        
+    }
+}
