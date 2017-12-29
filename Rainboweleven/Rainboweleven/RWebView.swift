981913b482141dd461ed89b8b70081ab7dbfc221
zhang <Angus@ZhangdeMacBook-Air.local>
2017-12-29 09:51:07 +0800

    支持storyboard

--- Rainboweleven/Rainboweleven/RWebView.swift
+++ Rainboweleven/Rainboweleven/RWebView.swift
@@ -41,11 +41,16 @@ open class RWebView: UIView,RWebViewProtocol {
     internal static let  INIT_SCRIPT = "function getJsBridge(){window._jsf=window._jsf||{};return{call:function(b,a,c){if('function'==typeof a){c=a;a={}}else{a={'param':a}}if('function'==typeof c){window.jscb=window.jscb||0;var d='jscb'+window.jscb++;window[d]=c;a._jscbstub=d}a=JSON.stringify(a||{});return window._jswk?prompt(window._jswk+b,a):'function'==typeof _jsBridge?_jsBridge(b,a):_jsBridge.call(b,a)},register:function(b,a){'object'==typeof b?Object.assign(window._jsf,b):window._jsf[b]=a}}}jsBridge=getJsBridge()"
     
     public required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
+        setUpWebView()
     }
     
     public override init(frame: CGRect) {
         super.init(frame: frame)
+        setUpWebView()
+    }
+    
+    fileprivate func setUpWebView() {
         //如果是iOS 8 以上系统就用 wkwebview 否用用 uiwebview
         if((UIDevice.current.systemVersion as NSString).floatValue >= 8.0){
             wv = RWKWebView(frame: frame)
